ebd123f8fbe8dd5de4b66d31adde75d79db73ac6
Thomas Qvidahl <thomas@qvidahl.no>
2024-10-14 14:23:20 +0200

    Implement simple test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+
 #[derive(Debug)]
 struct Point {
     x: f32,
@@ -7,6 +9,22 @@ struct Point {
 }
 
 fn main() {
-    let point = Point{x: 0.2, y:0.3, z:0.4, w:1};
+    let point = Point {
+        x: 0.2,
+        y: 0.3,
+        z: 0.4,
+        w: 1,
+    };
     print!("WTF, {:?}", point);
+}
+
+#[test]
+fn create_point() {
+    let point = Point {
+        x: 0.2,
+        y: 0.3,
+        z: 0.4,
+        w: 1,
+    };
+    assert!(point.x == 0.2 && point.y == 0.3 && point.z == 0.4 && point.w == 1);
 }
