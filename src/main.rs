112ea5f9c22669419ff550dcdf35d12c2335ace4
Thomas Qvidahl <thomas@qvidahl.no>
2024-10-14 14:09:09 +0200

    First naïve implementation of Point struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let point = Point{x: 0.2, y:0.3, z:0.4, w:1};
+    print!("WTF, {:?}", point);
 }
