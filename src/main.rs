e403133ece267e6fb0a8375f50771b30e10776df
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-08 16:10:24 +0100

    Implement Putting it together from Chapter 1.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
-use rtc::tuples::point;
+
+
+use rtc::tuples::{point, vector, Tuple};
 
 
 mod rtc;
 
 fn main() {
-    
-    let point = point(0.2, 0.3, 0.4);
-    print!("WTF, {:?}", point);
+    // Putting it together Chapter 1
+    #[derive(Debug)]
+    struct Projectile {
+        position: Tuple,
+        velocity: Tuple,
+    }
+
+    impl Projectile {
+        fn new(position: Tuple, velocity: Tuple) -> Self {
+            Self {position, velocity}
+        }
+    }
+
+    struct Environment {
+        gravity: Tuple,
+        wind: Tuple,
+    }
+
+    impl Environment {
+        fn new(gravity: Tuple, wind: Tuple) -> Self {
+            Self {gravity, wind}
+        }
+    }
+
+    fn tick(env: Environment, proj: Projectile) -> Projectile {
+        let pos = proj.position + proj.velocity;
+        let vel = proj.velocity + env.gravity + env.wind;
+
+        return Projectile::new(pos, vel);
+    }
+
+    let p = Projectile::new(point(0.0, 1.0, 0.0), vector(1.0, 1.0, 0.0).normalize());
+    let e = Environment::new(vector(0.0, -0.1,0.0), vector(-0.01, 0.0, 0.0));
+
+
+    print!("WTF, {:?}", p);
 }
