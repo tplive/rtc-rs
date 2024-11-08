602685f5bfabd73e99473f9fd378b945b358d802
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-08 14:33:58 +0100

    Implement division. Refactor tests to separate file.

--- src/rtc/tuples.rs
+++ src/rtc/tuples.rs
@@ -57,7 +57,7 @@ impl ops::Neg for Tuple {
 impl ops::Add for Tuple {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    fn add(self, other: Self) -> Self::Output {
         Self::new(
             self.x + other.x,
             self.y + other.y,
@@ -70,7 +70,7 @@ impl ops::Add for Tuple {
 impl ops::Sub for Tuple {
     type Output = Self;
 
-    fn sub(self, other: Self) -> Self {
+    fn sub(self, other: Self) -> Self::Output {
         Self::new(
             self.x - other.x,
             self.y - other.y,
@@ -80,125 +80,31 @@ impl ops::Sub for Tuple {
     }
 }
 
-#[cfg(test)]
-mod tests {
+impl ops::Mul<f32> for Tuple {
+    type Output = Self;
 
-    use crate::rtc::tuples::{point, vector, Tuple};
-
-    #[test]
-    fn test_point() {
-        let p = point(0.2, 0.3, -2.0);
-        assert_eq!(p.w, 1.0); // Ensure w is 1 for points.
-        assert!(p.is_point());
-        assert!(!p.is_vector());
-        assert!(p.x == 0.2 && p.y == 0.3 && p.z == -2.0); // Ensure values match the input.
-    }
-
-    #[test]
-    fn test_vector() {
-        let v = vector(-0.4, 1.1, 2.0);
-        assert_eq!(v.w, 0.0); // Ensure w is 0 for vectors.
-        assert!(v.is_vector());
-        assert!(!v.is_point());
-        assert!(v.x == -0.4 && v.y == 1.1 && v.z == 2.0); // Ensure values match the input.
-    }
-
-    #[test]
-    fn a_tuple_with_w_1_0_is_a_point() {
-        // This is the test from p. 4 of the book.
-
-        let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
-        assert_eq!(a.x, 4.3);
-        assert_eq!(a.y, -4.2);
-        assert_eq!(a.z, 3.1);
-        assert_eq!(a.w, 1.0);
-        assert!(a.is_point());
-        assert!(!a.is_vector());
-    }
-
-    #[test]
-    fn a_tuple_with_w_0_0_is_a_vector() {
-        // This is the test from p. 4 of the book.
-
-        let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
-        assert_eq!(a.x, 4.3);
-        assert_eq!(a.y, -4.2);
-        assert_eq!(a.z, 3.1);
-        assert_eq!(a.w, 0.0);
-        assert!(!a.is_point());
-        assert!(a.is_vector());
-    }
-
-    #[test]
-    fn point_creates_tuple_with_w_1_0() {
-        let p = point(4.0, -4.0, 3.0);
-        assert_eq!(p.w, 1.0);
-        assert!(p.is_point());
-        assert!(!p.is_vector());
-    }
-
-    #[test]
-    fn vector_creates_tuple_with_w_0_0() {
-        let v = vector(4.0, -4.0, 3.0);
-        assert_eq!(v.w, 0.0);
-        assert!(!v.is_point());
-        assert!(v.is_vector());
-    }
-
-    #[test]
-    fn adding_two_tuples() {
-        // Test from p. 6 in the book
-        let p = Tuple::new(3.0, -2.0, 5.0, 1.0);
-        let v = Tuple::new(-2.0, 3.0, 1.0, 0.0);
-        let expected_result = Tuple::new(1.0, 1.0, 6.0, 1.0);
-        let actual_result = p + v;
-        assert!(expected_result.eq(&actual_result));
-    }
-
-    #[test]
-    fn subtract_two_points() {
-        // Test from p. 6 in the book
-        let p = point(3.0, 2.0, 1.0);
-        let v = point(5.0, 6.0, 7.0);
-        let expected_result = vector(-2.0, -4.0, -6.0);
-        let actual_result = p - v;
-        assert!(expected_result.eq(&actual_result));
-    }
-
-    #[test]
-    fn adding_two_points_should_fail() {
-        let p1 = point(1.0, 2.0, 3.0);
-        let p2 = point(3.0, 2.0, 1.0);
-        assert_ne!(point(4.0, 4.0, 4.0), p1 + p2);
-    }
-
-    #[test]
-    fn subtract_vector_from_point() {
-        // Test from p. 6
-        let p = point(3.0, 2.0, 1.0);
-        let v = vector(5.0, 6.0, 7.0);
-        assert_eq!(point(-2.0, -4.0, -6.0), p - v);
-    }
-
-    #[test]
-    fn subtract_two_vectors() {
-        // Test from p. 7
-        let v1 = vector(3.0, 2.0, 1.0);
-        let v2 = vector(5.0, 6.0, 7.0);
-        assert_eq!(vector(-2.0, -4.0, -6.0), v1 - v2);
-    }
-
-    #[test]
-    fn subtract_vector_from_zero_vector() {
-        // Test from p. 7
-        let zero = vector(0.0,0.0,0.0);
-        let v = vector(1.0, -2.0, 3.0);
-        assert_eq!(vector(-1.0, 2.0, -3.0), zero - v);
-    }
-
-    #[test]
-    fn negate_tuple() {
-        let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
-        assert_eq!(Tuple::new(-1.0, 2.0, -3.0, 4.0), -t);
+    fn mul(self, scalar: f32) -> Self::Output {
+        Self::new(
+            self.x * scalar,
+            self.y * scalar,
+            self.z * scalar,
+            self.w * scalar,
+        )
     }
 }
+
+impl ops::Div<f32> for Tuple {
+    type Output = Self;
+
+    fn div(self, scalar: f32) -> Self::Output {
+        Self::new(
+            self.x / scalar,
+            self.y / scalar,
+            self.z / scalar,
+            self.w / scalar,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/rtc/tuples/tests.rs
@@ -0,0 +1,148 @@
+use crate::rtc::tuples::{point, vector, Tuple};
+
+#[test]
+fn test_point() {
+    let p = point(0.2, 0.3, -2.0);
+    assert_eq!(p.w, 1.0); // Ensure w is 1 for points.
+    assert!(p.is_point());
+    assert!(!p.is_vector());
+    assert!(p.x == 0.2 && p.y == 0.3 && p.z == -2.0); // Ensure values match the input.
+}
+
+#[test]
+fn test_vector() {
+    let v = vector(-0.4, 1.1, 2.0);
+    assert_eq!(v.w, 0.0); // Ensure w is 0 for vectors.
+    assert!(v.is_vector());
+    assert!(!v.is_point());
+    assert!(v.x == -0.4 && v.y == 1.1 && v.z == 2.0); // Ensure values match the input.
+}
+
+#[test]
+fn a_tuple_with_w_1_0_is_a_point() {
+    // This is the test from p. 4 of the book.
+
+    let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 1.0);
+    assert!(a.is_point());
+    assert!(!a.is_vector());
+}
+
+#[test]
+fn a_tuple_with_w_0_0_is_a_vector() {
+    // This is the test from p. 4 of the book.
+
+    let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 0.0);
+    assert!(!a.is_point());
+    assert!(a.is_vector());
+}
+
+#[test]
+fn point_creates_tuple_with_w_1_0() {
+    let p = point(4.0, -4.0, 3.0);
+    assert_eq!(p.w, 1.0);
+    assert!(p.is_point());
+    assert!(!p.is_vector());
+}
+
+#[test]
+fn vector_creates_tuple_with_w_0_0() {
+    let v = vector(4.0, -4.0, 3.0);
+    assert_eq!(v.w, 0.0);
+    assert!(!v.is_point());
+    assert!(v.is_vector());
+}
+
+#[test]
+fn adding_two_tuples() {
+    // Test from p. 6 in the book
+    let p = Tuple::new(3.0, -2.0, 5.0, 1.0);
+    let v = Tuple::new(-2.0, 3.0, 1.0, 0.0);
+    let expected_result = Tuple::new(1.0, 1.0, 6.0, 1.0);
+    let actual_result = p + v;
+    assert!(expected_result.eq(&actual_result));
+}
+
+#[test]
+fn subtract_two_points() {
+    // Test from p. 6 in the book
+    let p = point(3.0, 2.0, 1.0);
+    let v = point(5.0, 6.0, 7.0);
+    let expected_result = vector(-2.0, -4.0, -6.0);
+    let actual_result = p - v;
+    assert!(expected_result.eq(&actual_result));
+}
+
+#[test]
+fn adding_two_points_should_fail() {
+    let p1 = point(1.0, 2.0, 3.0);
+    let p2 = point(3.0, 2.0, 1.0);
+    assert_ne!(point(4.0, 4.0, 4.0), p1 + p2);
+}
+
+#[test]
+fn subtract_vector_from_point() {
+    // Test from p. 6
+    let p = point(3.0, 2.0, 1.0);
+    let v = vector(5.0, 6.0, 7.0);
+    assert_eq!(point(-2.0, -4.0, -6.0), p - v);
+}
+
+#[test]
+fn subtract_two_vectors() {
+    // Test from p. 7
+    let v1 = vector(3.0, 2.0, 1.0);
+    let v2 = vector(5.0, 6.0, 7.0);
+    assert_eq!(vector(-2.0, -4.0, -6.0), v1 - v2);
+}
+
+#[test]
+fn subtract_vector_from_zero_vector() {
+    // Test from p. 7
+    let zero = vector(0.0, 0.0, 0.0);
+    let v = vector(1.0, -2.0, 3.0);
+    assert_eq!(vector(-1.0, 2.0, -3.0), zero - v);
+}
+
+#[test]
+fn negate_tuple() {
+    // Test from p. 7
+    let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+    assert_eq!(Tuple::new(-1.0, 2.0, -3.0, 4.0), -t);
+}
+
+#[test]
+fn multiply_tuple_by_scalar() {
+    // Test from p. 8
+    let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+    let s: f32 = 3.5;
+    let expected = Tuple::new(3.5, -7.0, 10.5, -14.0);
+    let actual = t * s;
+    assert!(actual.eq(&expected)); 
+}
+#[test]
+fn multiply_tuple_by_fraction() {
+    // Test from p. 8
+    let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+    let s = 0.5;
+    let expected = Tuple::new(0.5, -1.0, 1.5,-2.0);
+    let actual = t * s;
+    assert!(actual.eq(&expected));
+}
+
+#[test]
+fn dividing_tuple_by_scalar() {
+// Test from p. 8
+let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+let s = 2.0;
+let expected = Tuple::new(0.5, -1.0, 1.5,-2.0);
+let actual = t / s;
+assert!(actual.eq(&expected));
+}
