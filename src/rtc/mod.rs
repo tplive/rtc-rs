a17ac50c042dcdac212b29d7e8fe9cebe08aee2f
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-13 10:25:16 +0100

    Refactor common functionality to top (rtc) module.

--- src/rtc/mod.rs
+++ src/rtc/mod.rs
@@ -1,2 +1,9 @@
 pub mod tuples;
 pub mod colors;
+
+const EPSILON: f32 = 0.00001_f32;
+
+fn equal(a: f32, b: f32) -> bool {
+    // Compare two f32 values for equality within the constant EPSILON
+    (a - b).abs() <= f32::EPSILON // Try built-in EPSILON value, but change to my constant if it doesn't work out.
+}
--- src/rtc/tuples/mod.rs
+++ src/rtc/tuples/mod.rs
@@ -3,12 +3,7 @@ mod tests;
 
 use std::ops;
 
-const EPSILON: f32 = 0.00001_f32;
-
-fn equal(a: f32, b: f32) -> bool {
-    // Compare two f32 values for equality within the constant EPSILON
-    (a - b).abs() <= f32::EPSILON // Try built-in EPSILON value, but change to my constant if it doesn't work out.
-}
+use super::equal;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Tuple {
