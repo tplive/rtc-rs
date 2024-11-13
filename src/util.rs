f1f17fba456e8a1b14751db513f67b8f20afd5fa
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-13 15:18:37 +0100

    Make type public

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 const EPSILON: f32 = 0.00001_f32;
 
-type RtcFl = f32;
+pub type RtcFl = f32;
 
 pub fn equal(a: f32, b: f32) -> bool {
     // Compare two f32 values for equality within the constant EPSILON
