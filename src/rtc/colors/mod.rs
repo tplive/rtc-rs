963bd0f8f5d74b4e69f3c18c24325b0f728f0517
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-13 10:25:35 +0100

    Implement color operations and tests.

--- src/rtc/colors/mod.rs
+++ src/rtc/colors/mod.rs
@@ -1,3 +1,10 @@
+#[cfg(test)]
+mod tests;
+
+use std::ops;
+use super::equal;
+
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
     red: f32,
     green: f32,
@@ -6,7 +13,65 @@ pub struct Color {
 
 impl Color {
     fn new(red: f32, green: f32, blue: f32) -> Self {
-        Self {red: red, green: green, blue: blue}
+        Self {
+            red: red,
+            green: green,
+            blue: blue,
+        }
+    }
+}
+
+impl ops::Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self::new(
+            self.red + other.red,
+            self.green + other.green,
+            self.blue + other.blue,
+        )
+    }
+}
+
+impl ops::Sub for Color {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self::new(
+            self.red - other.red,
+            self.green - other.green,
+            self.blue - other.blue,
+        )
+    }
+}
+
+impl ops::Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, other: f32) -> Self::Output {
+        Self::new(
+            self.red * other,
+            self.green * other,
+            self.blue * other,
+        )
+    }
+}
+
+impl ops::Mul<Color> for Color {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self::new(
+            self.red * other.red,
+            self.green * other.green,
+            self.blue * other.blue,
+        )
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, &other: &Color) -> bool {
+        equal(self.red, other.red) && equal(self.green, other.green) && equal(self.blue, other.blue)
     }
 }
 
@@ -14,5 +79,4 @@ pub fn color(r: f32, g: f32, b: f32) -> Color {
     Color::new(r, g, b)
 }
 
-#[cfg(test)]
-mod tests;
+
--- src/rtc/colors/tests.rs
+++ src/rtc/colors/tests.rs
@@ -7,3 +7,47 @@ fn colors_are_rgb_tuples() {
     assert_eq!(c.green, 0.4);
     assert_eq!(c.blue, 1.7);
 }
+
+#[test]
+fn adding_colors() {
+    let c1 = color(0.9, 0.6, 0.75);
+    let c2 = color(0.7, 0.1, 0.25);
+
+    let expected_result = color(1.6, 0.7, 1.0);
+    let actual_result = c1 + c2;
+
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn subtracting_colors() {
+    let c1 = color(0.9, 0.6, 0.75);
+    let c2 = color(0.7, 0.1, 0.25);
+
+    let expected_result = color(0.2, 0.5, 0.5);
+    let actual_result = c1 - c2;
+
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn multiply_color_by_scalar() {
+    let c1 = color(0.2, 0.3, 0.4);
+    let multiplier = 2.0;
+
+    let expected_result = color(0.4, 0.6, 0.8);
+    let actual_result = c1 * multiplier;
+
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn multiply_two_colors() {
+    let c1 = color(1.0, 0.2, 0.4);
+    let c2 = color(0.9, 1.0, 0.1);
+
+    let expected_result = color(0.9, 0.2, 0.04);
+    let actual_result = c1 * c2;
+
+    assert_eq!(expected_result, actual_result);
+}
