ad943ecf9515091174d7572628e06f8dd1f4f0ed
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-14 11:00:05 +0100

    Implement pixel data and tests.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,43 +1,39 @@
-use std::ops::Index;
-
-use crate::{colors::Color, util::RtcFl};
-
+use crate::colors::Color;
+#[derive(Debug, Clone)]
 pub struct Canvas {
     pub width: usize,
     pub height: usize,
-    data: Vec<RtcFl>,
-    bits: usize,
+    data: Vec<Color>,
 }
 
 impl Canvas {
     pub fn new(width: usize, height: usize) -> Self {
 
-        let bits: usize = 3;
-        let mut data: Vec<RtcFl> = vec![0.0; width * height * bits];
+        let data: Vec<Color> = vec![Color::black(); width * height];
 
         Self {
             width,
             height,
             data,
-            bits,
         }
     }
 
-    pub fn bits(&self) -> usize { self.bits}
-
-    pub fn write_pixel(&self, x: usize, y: usize, color: Color) {
-        let index: usize = (y * self.width + x) * self.bits;
-        &mut self.data.index(index) = color.red;
-        &mut self.data[index+1] = color.green;
-        &mut self.data[index+2] = color.blue;
+    pub fn data_size(&self) -> usize {
+        self.data.len()
     }
 
-    pub fn pixel_at(&self, x, y) -> Color {
-        let index = y * self.width + x;
-        let r = self.data[index];
-        let g = self.data[index+1];
-        let b = self.data[index+2];
-        Color::new(r, g, b)
+    pub fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
+        let index: usize = y * self.width + x;
+        self.data[index] = color;
     }
 
+    pub fn pixel_at(&self, x: usize, y: usize) -> &Color {
+        &self.data[y * self.width + x]
+    }
+
+    pub fn to_ppm(&self) -> String {
+        let head = format!("P3\n{} {}\n255\n", &self.width.to_string(), &self.height.to_string());
+        String::from(head)
+    }
 }
+
--- src/canvas_tests.rs
+++ src/canvas_tests.rs
@@ -1,40 +1,50 @@
-use crate::{canvas::Canvas, colors::{color, Color}, util::RtcFl};
-
-
 #[cfg(test)]
+use crate::{canvas::Canvas, colors::Color};
+
 
 #[test]
 fn creating_a_canvas() {
-    use crate::{canvas::Canvas, util::RtcFl};
+    use crate::canvas::Canvas;
 
-
-    let bits: usize = 3;
     let w = 10;
     let h = 20;
-    let black: RtcFl = 0.0;
 
     let c = Canvas::new(w, h);
-
-    let is_black = c.data.iter().all(|i: &RtcFl| i == &black);
+    let color = Color::black();
+    for x in 0..c.width -1 {
+        for y in 0..c.height -1 {
+            assert!(*c.pixel_at(x, y) == color);
+        }
+    }
+    //let is_black = c.data.iter().all(|i: &RtcFl| i == &black);
     
     assert_eq!(c.width, w);
     assert_eq!(c.height, h);
-    assert_eq!(c.data.len(), w as usize * h as usize * bits);
+    assert_eq!(c.data_size(), w as usize * h as usize);
     
-    assert_eq!(c.data.capacity(), w as usize * h as usize * bits);
-    assert!(is_black);
 }
 
 #[test]
 fn writing_pixels_to_a_canvas() {
     
-    let w = 10;
-    let h = 20;
+    let w: usize = 10;
+    let h: usize = 20;
+    let color: Color = Color::new(0.2, 0.4, 0.6);
+    let mut c: Canvas = Canvas::new(w, h);
 
-    let c = Canvas::new(w, h);
+    c.write_pixel(3, 3, color);
 
-    c.write_pixel(3, 3, color(0.2, 0.4, 0.6));
+    let result: &Color = c.pixel_at(3, 3);
 
-    assert!(c.pixel_at(3, 3).eq(&Color::new(0.2, 0.4, 0.6)));
+    assert!(result == &color);
     
+}
+
+#[test]
+fn constructing_ppm_header() {
+    let c = Canvas::new(5, 3);
+    
+    let ppm = c.to_ppm();
+
+    assert_eq!(ppm, String::from("P3\n5 3\n255\n"));
 }
