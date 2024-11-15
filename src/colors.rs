ead709696111c5dfe123831944cce0a594268f48
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-15 11:39:38 +0100

    Add white

--- src/colors.rs
+++ src/colors.rs
@@ -21,6 +21,10 @@ impl Color {
     pub fn black() -> Self {
         Self::new(0.0, 0.0, 0.0)
     }
+    
+    pub fn white() -> Self {
+        Self::new(1.0, 1.0, 1.0)
+    }
 }
 
 impl ops::Add for Color {
