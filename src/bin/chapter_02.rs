fbee54e8d639e9ca15540a65b67a848104af928c
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-19 11:41:29 +0100

    Testing save to PPM file and plotting projectile on canvas.

--- src/bin/chapter_02.rs
+++ src/bin/chapter_02.rs
@@ -2,7 +2,11 @@ extern crate rtc_rs as rtc;
 
 use std::{fs::File, io::Write};
 
-use rtc::{color::Color, canvas::Canvas, tuples::{point, vector, Tuple}};
+use rtc::{
+    canvas::Canvas,
+    color::Color,
+    tuples::{point, vector, Tuple},
+};
 
 // Putting it together Chapter 2
 #[derive(Debug)]
@@ -13,7 +17,7 @@ struct Projectile {
 
 impl Projectile {
     fn new(position: Tuple, velocity: Tuple) -> Self {
-        Self {position, velocity}
+        Self { position, velocity }
     }
 }
 struct Environment {
@@ -23,44 +27,49 @@ struct Environment {
 
 impl Environment {
     fn new(gravity: Tuple, wind: Tuple) -> Self {
-        Self {gravity, wind}
+        Self { gravity, wind }
     }
 }
 
 fn tick(env: &Environment, proj: Projectile) -> Projectile {
-    let pos = proj.position + proj.velocity;
     let vel = proj.velocity + env.gravity + env.wind;
+    let pos = proj.position + proj.velocity;
 
     return Projectile::new(pos, vel);
 }
 
 fn main() {
+    let velocity = vector(1.0, 1.8, 0.0);
 
-    let velocity = vector(1.0, 1.8, 0.0) * 11.25;
+    let mut p = Projectile::new(point(0.0, 1.0, 0.0), velocity);
+    let e = Environment::new(vector(0.0, -0.1, 0.0), vector(-0.01, 0.0, 0.0));
 
-
-    let mut p = Projectile::new(point(0.0, 1.0, 0.0), velocity.normalize());
-    let e = Environment::new(vector(0.0, -0.1,0.0), vector(-0.01, 0.0, 0.0));
-
-    let mut can = Canvas::new(900, 550);
+    let mut can = Canvas::new(300, 300);
+    let mut values: Vec<(usize, usize)> = vec![];
 
     while p.position.y >= 0.0 {
+                
+        let x = can.width as f32 - p.position.x * can.width as f32;
+        let y = can.height as f32 - p.position.y * can.height as f32;
+        
+        values.append(&mut vec![(x as usize, y as usize)]);
+        println!(
+            "Position values:\t{:?}\t{:?}",
+            p.position.x, p.position.y
+        );
 
         p = tick(&e, p);
-
-        let p_n = p.position.normalize();
-
-        let x = can.width as f32 - p_n.x * can.width as f32;
-        let y = can.height as f32 - p_n.y * can.height as f32;
-        can.write_pixel(x as usize, y as usize, Color::white());
-
-        println!("Position on canvas: {:?} {:?}", x as usize, y as usize);
-        
     }
 
-    let mut file= File::create("chapter_01.ppm").expect("Unable to create file.");
+    for (x, y) in values {
+        can.write_pixel(x, y, Color::white());
+        println!("Position on canvas:\t{:?}\t\t{:?}", x, y);
+    }
 
-        file.write_all(&can.to_ppm().as_bytes()).expect("Unable to write file.");
+    let mut file = File::create("chapter_02.ppm").expect("Unable to create file.");
+
+    file.write_all(&can.to_ppm().as_bytes())
+        .expect("Unable to write file.");
 
     //print!("{}", can.to_ppm());
 }
