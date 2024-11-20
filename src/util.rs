898df554c0810f34f2417df7a79dec08621ea007
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-20 15:01:37 +0100

    Fix type definition.

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,9 @@
-const EPSILON: f32 = 0.0001_f32;
 
 pub type RtcFl = f32;
+const EPSILON: RtcFl = 0.0001;
 
-pub fn equal(a: f32, b: f32) -> bool {
+
+pub fn equal(a: RtcFl, b: RtcFl) -> bool {
     // Compare two f32 values for equality within the constant EPSILON
     (a - b).abs() <= EPSILON
 }
