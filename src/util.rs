cf0c6cbf091c925c10aa9f13d2631832c60cf2a6
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-20 14:22:42 +0100

    Reinstating my own EPSILON value.

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,8 @@
-//const EPSILON: f32 = 0.00001_f32;
+const EPSILON: f32 = 0.0001_f32;
 
 pub type RtcFl = f32;
 
 pub fn equal(a: f32, b: f32) -> bool {
     // Compare two f32 values for equality within the constant EPSILON
-    (a - b).abs() <= f32::EPSILON // Try built-in EPSILON value, but change to my constant if it doesn't work out.
+    (a - b).abs() <= EPSILON
 }
