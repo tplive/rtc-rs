13fcb2da7eea9f55fab8967453410ee94325b7e0
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-20 10:07:21 +0100

    Fix broken calculations in chapter02

--- src/bin/chapter_02.rs
+++ src/bin/chapter_02.rs
@@ -39,20 +39,19 @@ fn tick(env: &Environment, proj: Projectile) -> Projectile {
 }
 
 fn main() {
-    let velocity = vector(1.0, 1.8, 0.0);
 
-    let mut p = Projectile::new(point(0.0, 1.0, 0.0), velocity);
+    let mut p = Projectile::new(point(0.0, 1.0, 0.0), vector(1.0, 1.8, 0.0).normalize() * 11.25);
     let e = Environment::new(vector(0.0, -0.1, 0.0), vector(-0.01, 0.0, 0.0));
 
-    let mut can = Canvas::new(300, 300);
+    let mut can = Canvas::new(900, 500);
     let mut values: Vec<(usize, usize)> = vec![];
-
+ 
     while p.position.y >= 0.0 {
                 
-        let x = can.width as f32 - p.position.x * can.width as f32;
-        let y = can.height as f32 - p.position.y * can.height as f32;
+        let x = p.position.x as usize;
+        let y = can.height - p.position.y as usize;
         
-        values.append(&mut vec![(x as usize, y as usize)]);
+        values.append(&mut vec![(x, y as usize)]);
         println!(
             "Position values:\t{:?}\t{:?}",
             p.position.x, p.position.y
@@ -65,11 +64,10 @@ fn main() {
         can.write_pixel(x, y, Color::white());
         println!("Position on canvas:\t{:?}\t\t{:?}", x, y);
     }
-
+ 
     let mut file = File::create("chapter_02.ppm").expect("Unable to create file.");
 
     file.write_all(&can.to_ppm().as_bytes())
         .expect("Unable to write file.");
 
-    //print!("{}", can.to_ppm());
 }
