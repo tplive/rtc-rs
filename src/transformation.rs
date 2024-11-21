758da509a7b91fe4323a6e5c510222aa0bf73b4f
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-21 13:32:28 +0100

    Refactoring. Implement shortcut functions.

--- src/transformation.rs
+++ src/transformation.rs
@@ -1,5 +1,5 @@
-use crate::util::RtcFl;
 use crate::matrix::Matrix4x4;
+use crate::util::RtcFl;
 
 pub struct Transformation {}
 
@@ -22,13 +22,13 @@ impl Transformation {
         m
     }
 
-    pub fn rotation_x(rad: RtcFl) -> Matrix4x4 {
+    pub fn rotation_x(r: RtcFl) -> Matrix4x4 {
         let mut m = Matrix4x4::identity();
-        m[(1, 1)] = rad.cos();
-        m[(1, 2)] = -rad.sin();
-        m[(2, 1)] = rad.sin();
-        m[(2, 2)] = rad.cos();
-        
+        m[(1, 1)] = r.cos();
+        m[(1, 2)] = -r.sin();
+        m[(2, 1)] = r.sin();
+        m[(2, 2)] = r.cos();
+
         m
     }
 
@@ -38,7 +38,7 @@ impl Transformation {
         m[(0, 2)] = r.sin();
         m[(2, 0)] = -r.sin();
         m[(2, 2)] = r.cos();
-        
+
         m
     }
 
@@ -48,11 +48,11 @@ impl Transformation {
         m[(0, 1)] = -r.sin();
         m[(1, 0)] = r.sin();
         m[(1, 1)] = r.cos();
-        
+
         m
     }
 
-    pub fn shearing(xy: RtcFl, xz: RtcFl, yx: RtcFl, yz: RtcFl, zx: RtcFl, zy: RtcFl, ) -> Matrix4x4 {
+    pub fn shearing(xy: RtcFl, xz: RtcFl, yx: RtcFl, yz: RtcFl, zx: RtcFl, zy: RtcFl) -> Matrix4x4 {
         let mut m = Matrix4x4::identity();
         m[(0, 1)] = xy;
         m[(0, 2)] = xz;
@@ -60,8 +60,32 @@ impl Transformation {
         m[(1, 2)] = yz;
         m[(2, 0)] = zx;
         m[(2, 1)] = zy;
-        
+
         m
     }
-    
+}
+
+// Shortcut functions
+pub fn tr(x: RtcFl, y: RtcFl, z: RtcFl) -> Matrix4x4 {
+    Transformation::translation(x, y, z)
+}
+
+pub fn sc(x: RtcFl, y: RtcFl, z: RtcFl) -> Matrix4x4 {
+    Transformation::scaling(x, y, z)
+}
+
+pub fn rx(r: RtcFl) -> Matrix4x4 {
+    Transformation::rotation_x(r)
+}
+
+pub fn ry(r: RtcFl) -> Matrix4x4 {
+    Transformation::rotation_y(r)
+}
+
+pub fn rz(r: RtcFl) -> Matrix4x4 {
+    Transformation::rotation_z(r)
+}
+
+pub fn sh(xy: RtcFl, xz: RtcFl, yx: RtcFl, yz: RtcFl, zx: RtcFl, zy: RtcFl) -> Matrix4x4 {
+    Transformation::shearing(xy, xz, yx, yz, zx, zy)
 }
