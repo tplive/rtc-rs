b2fb3e4d60feeb1aba9c25948a987b8b1bd44977
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-21 12:52:17 +0100

    Implement rotation xyz transformations.

--- src/transformation.rs
+++ src/transformation.rs
@@ -21,4 +21,35 @@ impl Transformation {
 
         m
     }
+
+    pub fn rotation_x(rad: RtcFl) -> Matrix4x4 {
+        let mut m = Matrix4x4::identity();
+        m[(1, 1)] = rad.cos();
+        m[(1, 2)] = -rad.sin();
+        m[(2, 1)] = rad.sin();
+        m[(2, 2)] = rad.cos();
+        
+        m
+    }
+
+    pub fn rotation_y(r: RtcFl) -> Matrix4x4 {
+        let mut m = Matrix4x4::identity();
+        m[(0, 0)] = r.cos();
+        m[(0, 2)] = r.sin();
+        m[(2, 0)] = -r.sin();
+        m[(2, 2)] = r.cos();
+        
+        m
+    }
+
+    pub fn rotation_z(r: RtcFl) -> Matrix4x4 {
+        let mut m = Matrix4x4::identity();
+        m[(0, 0)] = r.cos();
+        m[(0, 1)] = -r.sin();
+        m[(1, 0)] = r.sin();
+        m[(1, 1)] = r.cos();
+        
+        m
+    }
+    
 }
