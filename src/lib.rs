7ba46cf5a1c6a25ff7ca472f3757aa40ab5faff0
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-22 13:01:11 +0100

    Annotate test modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
 pub mod tuples;
+#[cfg(test)]
 pub mod tuple_tests;
 
 pub mod color;
+#[cfg(test)]
 pub mod color_tests;
 
 pub mod canvas;
+#[cfg(test)]
 pub mod canvas_tests;
 
 pub mod matrix;
+#[cfg(test)]
 pub mod matrix_tests;
 
 pub mod transformation;
+#[cfg(test)]
 pub mod transformation_tests;
 
 pub mod util;
