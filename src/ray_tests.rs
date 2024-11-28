4a4c97af5b0a1447d517174096b62e84a3caa84c
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-28 15:37:08 +0100

    Remove redundant brackets.

--- src/ray_tests.rs
+++ src/ray_tests.rs
@@ -98,8 +98,8 @@ fn aggregating_intersections() {
     let s = Sphere::new();
     let i1 = Intersection::new(1.0, &s);
     let i2 = Intersection::new(2.0, &s);
-    let xs = vec![[i1, i2]];
-
+    let xs = vec!(&i1, &i2);
+    
     assert_eq!(xs.len(), 2);
     assert_eq!(xs[0].t, 1.0);
     assert_eq!(xs[1].t, 2.0);
