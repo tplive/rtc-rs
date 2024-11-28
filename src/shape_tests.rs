cd5dc1c4213397bad45f66cf95e434490708793f
Thomas Qvidahl <thomas@qvidahl.no>
2024-11-28 15:36:26 +0100

    Change test to check that id's are unique and sequential.

--- src/shape_tests.rs
+++ src/shape_tests.rs
@@ -4,14 +4,16 @@ use crate::shape::Sphere;
 #[test]
 fn creating_a_sphere() {
 
+    // Since id's are given by the global static function in utils.rs, and tests apparently run
+    // async, there is no guarantee that the id's will be 1, 2, 3. Instead, we ensure that they are
+    // unique and sequential.
+    let s1 = Sphere::new(); // .id = n
+    let s2 = Sphere::new(); // .id = n + 1
+    let s3 = Sphere::new(); // .id = n + 2
 
-    let s1 = Sphere::new();
-    let s2 = Sphere::new();
-    let s3 = Sphere::new();
+    let n = s1.id;
 
-    assert_eq!(s1.id, 1);
-    assert_eq!(s2.id, 2);
-    assert_eq!(s3.id, 3);
-
-    
+    assert_eq!(s1.id, n);
+    assert_eq!(s2.id, n+1);
+    assert_eq!(s3.id, n+2);
 }
