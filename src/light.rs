f0a66e099678eba58dd4e518292ee17596759037
Thomas Qvidahl <thomas@qvidahl.no>
2024-12-12 14:02:24 +0100

    Implements lighting as a "standalone" function. May not belong here.

--- src/light.rs
+++ src/light.rs
@@ -1,4 +1,4 @@
-use crate::{color::Color, tuples::Tuple};
+use crate::{color::Color, material::Material, tuples::Tuple};
 pub struct Light {
     pub position: Tuple,
     pub intensity: Color,
@@ -11,4 +11,39 @@ impl Light {
             intensity,
         }
     }
+}
+
+pub fn lighting(
+    material: Material,
+    light: Light,
+    point: Tuple,
+    eye_vector: Tuple,
+    normal_vector: Tuple,
+) -> Color {
+    let effective_color = material.color * light.intensity;
+    let light_vector = (light.position - point).normalize();
+    let ambient = effective_color * material.ambient;
+    let light_dot_normal = light_vector.dot(normal_vector);
+
+    let diffuse: Color;
+    let specular: Color;
+
+    if light_dot_normal < 0.0 {
+        diffuse = Color::black();
+        specular = Color::black();
+    } else {
+        diffuse = effective_color * material.diffuse * light_dot_normal;
+
+        let reflect_vector = -light_vector.reflect(normal_vector);
+        let reflect_dot_eye = reflect_vector.dot(eye_vector);
+
+        if reflect_dot_eye <= 0.0 {
+            specular = Color::black();
+        } else {
+            let factor = reflect_dot_eye.powf(material.shininess);
+            specular = light.intensity * material.specular * factor;
+        }
+    }
+
+    ambient + diffuse + specular
 }
