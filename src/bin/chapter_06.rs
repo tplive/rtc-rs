286f083aaa658b30296c719cae77d4ff00ba37d0
Thomas Qvidahl <thomas@qvidahl.no>
2024-12-13 12:37:36 +0100

    WIP: Rendering Phong shading. Still missing the specular highlight.

--- src/bin/chapter_06.rs
+++ src/bin/chapter_06.rs
@@ -1,17 +1,18 @@
 extern crate rtc_rs as rtc;
 
-use std::{fs::File, io::Write, time::Instant};
+use std::{fs::File, io::Write, time::{Duration, Instant}};
 
 use rtc::{
     canvas::Canvas,
     color::Color,
     light::{lighting, Light},
     ray::Ray,
-    shape::{Intersectable, Intersections, Shape, Sphere},
-    transformation::Transformation,
+    shape::{Intersectable, Intersections, NormalAt, Sphere},
     tuples::point,
     util::RtcFl,
 };
+
+use indicatif::ProgressBar;
 
 // Putting it together Chapter 6
 fn main() {
@@ -27,7 +28,7 @@ fn main() {
     let mut shape = Sphere::default();
     shape.material.color = Color::new(1.0, 0.2, 1.0);
 
-    let light = Light::point(point(-10.0, 10.0, -10.0), Color::white());
+    let light = Light::point(point(-10.0, 10.0, 10.0), Color::white());
 
     /*
     shape.transform = Transformation::new()
@@ -36,35 +37,45 @@ fn main() {
         .get();
     */
 
+    let bar = ProgressBar::new((canvas_pixels * canvas_pixels) as u64);
+    bar.enable_steady_tick(Duration::from_millis(500));
+    
     for y in 0..canvas_pixels - 1 {
         let world_y = half - pixel_size * y as RtcFl;
         for x in 0..canvas_pixels - 1 {
+            bar.inc(1);
             let world_x = -half + pixel_size * x as RtcFl;
             let position = point(world_x, world_y, wall_z);
-            let r = Ray::new(ray_origin, (position - ray_origin).normalize());
-            let xs = Intersections::new(shape.intersect(r));
-            if xs.hit() != None {
-                canvas.write_pixel(x, y, Color::black());
-            } else {
-                let the_hit = xs.hit().expect("Intersection not found");
+            let r = Ray::new(&ray_origin, &(position - ray_origin).normalize());
+            let xs = Intersections::new(shape.intersect(&r));
 
-                
-                let color = lighting(
-                    shape.material,
-                    light,
-                    r.position(the_hit.t),
-                    -r.direction,
-                    the_hit.shape.normal_at(position),
-                );
+            match xs.hit() {
+                Some(the_hit) => {
+                    let the_hit_normal = &the_hit.shape.normal_at(position);
+                    let color = lighting(
+                        &shape.material,
+                        &light,
+                        &r.position(the_hit.t),
+                        &-r.direction,
+                        &the_hit_normal,
+                    );
+                    canvas.write_pixel(x, y, color);
+                }
+
+                None => {
+                    canvas.write_pixel(x, y, Color::black());
+                }
             }
         }
     }
+
+    bar.finish();
 
     let elapsed = now.elapsed();
     println!("Elapsed time for rendering: {:.2?}", elapsed);
 
     // Write to file
-    let mut file = File::create("chapter_05.ppm").expect("Unable to create file.");
+    let mut file = File::create("chapter_06.ppm").expect("Unable to create file.");
 
     file.write_all(&canvas.to_ppm().as_bytes())
         .expect("Unable to write file.");
