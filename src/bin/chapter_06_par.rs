0af4c28bb3c965b934e9b1b832bc818638b01c51
Thomas Qvidahl <thomas@qvidahl.no>
2024-12-19 13:18:05 +0100

    Comment code.

--- src/bin/chapter_06_par.rs
+++ src/bin/chapter_06_par.rs
@@ -16,7 +16,10 @@ use indicatif::ProgressBar;
 
 // Putting it together Chapter 6
 fn main() {
+    // Start timing the run:
     let now = Instant::now();
+    
+    // Set up the scene
     let ray_origin = point(0.0, 0.0, -5.0);
     let wall_z: RtcFl = 10.0;
     let wall_size: RtcFl = 7.0;
@@ -24,17 +27,27 @@ fn main() {
     let pixel_size: RtcFl = wall_size / canvas_pixels as RtcFl;
     let half: RtcFl = wall_size / 2.0;
 
+    // Create a canvas and a shape, configure the shape
     let mut canvas = Canvas::new(canvas_pixels, canvas_pixels);
     let mut shape = Sphere::default();
     shape.material.color = Color::new(1.0, 0.2, 1.0);
+    shape.material.ambient = 0.1;
+    shape.material.diffuse = 0.9;
+    shape.material.specular = 0.2;
+    shape.material.shininess = 200.0;
 
+    // Set up the light source
     let light = Light::point(point(-10.0, 10.0, -10.0), Color::white());
 
+    // Add a progress bar
     let bar = ProgressBar::new((canvas_pixels * canvas_pixels) as u64);
     bar.enable_steady_tick(Duration::from_millis(250));
     
+    // Initialize parallelism
     let (tx, rx) = mpsc::channel();
     let num_threads = available_parallelism().map(|n| n.get()).unwrap_or(1);
+    
+    // Divide the work into chunks
     let mut pairs = Vec::new();
     for y in 0..canvas_pixels -1 {
         for x in 0..canvas_pixels - 1 {
@@ -45,6 +58,7 @@ fn main() {
     let chunk_size = pairs.len() / num_threads;
     let chunks: Vec<_> = pairs.chunks(chunk_size).collect();
 
+    // Run the threads
     for chunk in chunks {
         let tx = tx.clone();
         let light = light.clone();
@@ -54,12 +68,15 @@ fn main() {
 
         thread::spawn(move || {
             for (x, y) in chunk {
+                
+                // Calculate the world coordinates for the pixel
                 let world_y = half - pixel_size * y as RtcFl;
                 let world_x = -half + pixel_size * x as RtcFl;
                 let position = (point(world_x, world_y, wall_z) - ray_origin).normalize();
                 let r = Ray::new(&ray_origin, &position);
                 let xs = Intersections::new(shape.intersect(&r));
     
+                // Determine the color of the pixel
                 let color = match xs.hit() {
                     Some(the_hit) => {
                         let the_hit_normal = &the_hit.shape.normal_at(position);
@@ -75,21 +92,25 @@ fn main() {
                     None => Color::black(),
                 };
 
+                // Send pixel coordinates and color to the main thread
                 tx.send((x, y, color)).expect("Failed to send data.");
-                
             }
         });
     }
 
+    // Drop the transmitter
     drop(tx);
 
+    // Receive pixels and write to canvas
     for (x, y, color) in rx {
         bar.inc(1);
         canvas.write_pixel(x, y, color);
     }
 
+    // Finish the progress bar
     bar.finish();
 
+    // Calculate and print elapsed time and number of threads
     let elapsed = now.elapsed();
     println!("Number of threads: {}", num_threads);
     println!("Elapsed time for rendering: {:.2?}", elapsed);
