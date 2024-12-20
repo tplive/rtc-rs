319df85e834babcad37f387d7d87fda517ca13d2
Thomas Qvidahl <thomas@qvidahl.no>
2024-12-20 12:48:24 +0100

    Looks like we don't need tests in lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,11 @@
 pub mod tuples;
-#[cfg(test)]
-pub mod tuple_tests;
-
 pub mod color;
-#[cfg(test)]
-pub mod color_tests;
-
 pub mod canvas;
-#[cfg(test)]
-pub mod canvas_tests;
-
 pub mod matrix;
-#[cfg(test)]
-pub mod matrix_tests;
-
 pub mod transformation;
-#[cfg(test)]
-pub mod transformation_tests;
-
 pub mod ray;
-#[cfg(test)]
-pub mod ray_tests;
-
 pub mod shape;
-#[cfg(test)]
-pub mod shape_tests;
-
 pub mod light;
-#[cfg(test)]
-pub mod light_tests;
-
 pub mod material;
-#[cfg(test)]
-pub mod material_tests;
-
 pub mod util;
 
