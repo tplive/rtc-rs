829eff599a83fd27e82a9aa06546554c467729fc
Thomas Qvidahl <thomas@qvidahl.no>
2025-05-02 12:32:42 +0200

    Fix vector calculations.

--- src/bin/chapter_06.rs
+++ src/bin/chapter_06.rs
@@ -48,13 +48,15 @@ fn main() {
 
             match xs.hit() {
                 Some(the_hit) => {
-                    let the_hit_normal = &the_hit.shape.normal_at(position);
+                    let hit_point = r.position(the_hit.t);
+                    let normal_vector = &the_hit.shape.normal_at(hit_point);
+                    let eye_vector = -r.direction;
                     let color = lighting(
                         &shape.material,
                         &light,
-                        &r.position(the_hit.t),
-                        &-r.direction,
-                        the_hit_normal,
+                        &hit_point,
+                        &eye_vector,
+                        normal_vector,
                     );
                     canvas.write_pixel(x, y, color);
                 }
