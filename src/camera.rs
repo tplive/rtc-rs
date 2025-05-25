e25fe5018a6678649ccbfc40eaf8fb56eeae36ba
Thomas Qvidahl <thomas@qvidahl.no>
2025-05-25 12:19:17 +0200

    Implements ray_for_pixel

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,4 @@
-use crate::{matrix::Matrix4, util::RtcFl};
+use crate::{matrix::Matrix4, ray::Ray, tuples::point, util::RtcFl};
 
 pub struct Camera {
     pub hsize: usize,
@@ -17,8 +17,8 @@ impl Camera {
         let half_view = (fov / 2.0).tan();
         let aspect = hsize as RtcFl / vsize as RtcFl;
 
-        let half_width:RtcFl;
-        let half_height:RtcFl;
+        let half_width: RtcFl;
+        let half_height: RtcFl;
 
         if aspect >= 1.0 {
             half_width = half_view;
@@ -40,12 +40,26 @@ impl Camera {
             half_height,
         }
     }
+}
 
+pub fn ray_for_pixel(camera: Camera, px: usize, py: usize) -> Ray {
+    // the offset from the edge of the canvas to the pixel's center
+    let xoffset = (px as RtcFl + 0.5) * camera.pixel_size;
+    let yoffset = (py as RtcFl + 0.5) * camera.pixel_size;
+
+    let world_x = camera.half_width - xoffset;
+    let world_y = camera.half_height - yoffset;
+
+    let pixel = camera.transform.try_inverse().unwrap() * point(world_x, world_y, -1.0);
+    let origin = camera.transform.try_inverse().unwrap() * point(0.0, 0.0, 0.0);
+    let direction = (pixel - origin).normalize();
+
+    Ray::new(&origin, &direction)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{camera::Camera, matrix::Matrix4, util::PI};
+    use crate::{camera::{ray_for_pixel, Camera}, matrix::Matrix4, tuples::{point, vector}, util::PI};
 
     #[test]
     fn constructing_camera() {
@@ -72,5 +86,14 @@ mod tests {
         let c = Camera::new(125, 200, PI / 2.0);
 
         assert_eq!(c.pixel_size, 0.01);
+    }
+
+    #[test]
+    fn construct_ray_through_center_of_canvas() {
+        let c = Camera::new(201, 101, PI / 2.0);
+        let r = ray_for_pixel(c, 100, 50);
+
+        assert_eq!(r.origin, point(0.0, 0.0, 0.0));
+        assert_eq!(r.direction, vector(0.0, 0.0, -1.0));
     }
 }
