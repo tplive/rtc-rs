d34bcf1f323f1a06567e05275a42983a3403968b
Thomas Qvidahl <thomas@qvidahl.no>
2025-05-30 13:19:01 +0200

    Sort lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,19 @@
-
-pub mod tuples;
-
-
-pub mod color;
-
-pub mod canvas;
-
-pub mod matrix;
-
-pub mod transformation;
-
-pub mod ray;
-
-pub mod shape;
-
-pub mod sphere;
-
-pub mod light;
-
-pub mod material;
-
-pub mod world;
-
-pub mod computation;
-
-pub mod intersections;
-
+// Lib module loads all application modules.
+// Tests have been moved inside each module.
+// Keep alphabetical because CDO.
 pub mod camera;
-
+pub mod canvas;
+pub mod color;
+pub mod computation;
+pub mod intersections;
+pub mod light;
+pub mod material;
+pub mod matrix;
+pub mod ray;
 pub mod render;
-
+pub mod shape;
+pub mod sphere;
+pub mod transformation;
+pub mod tuples;
 pub mod util;
+pub mod world;
