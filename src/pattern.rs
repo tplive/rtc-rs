4c3d68ece991d212539fef55edf6d0d34650f24b
Thamsen <thomas@bzn.no>
2025-10-28 09:45:04 +0100

    Implements pattern abstracted. Fixes naming conflict. Tests run fine.

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,21 +1,64 @@
-use crate::{color::Color, tuples::Tuple};
+use std::any::Any;
 
+use crate::{color::Color, shape::Shape, tuples::Tuple};
+
+pub enum PatternEnum {
+    StripePattern(Color, Color),
+}
+
+pub trait Pattern: Send + Sync + std::fmt::Debug + Any {
+    fn pattern_at(&self, world_point: Tuple) -> Color;
+    fn pattern_at_object(&self, shape: &dyn Shape, object_point: Tuple) -> Color;
+    fn eq_pattern(&self, other: &dyn Pattern) -> bool;
+    fn clone_boxed(&self) -> Box<dyn Pattern>;
+    fn as_any(&self) -> &dyn Any;
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct StripePattern {
     pub a: Color,
     pub b: Color,
 }
 
 impl StripePattern {
-    fn new(a: Color, b: Color) -> Self {
+    pub fn new(a: Color, b: Color) -> Self {
         Self { a, b }
     }
+}
 
-    pub fn stripe_at(&self, point: Tuple) -> Color {
+impl Pattern for StripePattern {
+    fn pattern_at(&self, point: Tuple) -> Color {
+
         if point.x.floor().abs() as usize % 2 == 0 {
             self.a
         } else {
             self.b
         }
+    }
+
+    fn pattern_at_object(&self, shape: &dyn Shape, world_point: Tuple) -> Color {
+        let object_point = shape
+            .transform()
+            .try_inverse()
+            .expect("Shape transform must be invertible for pattern calculation")
+            * world_point;
+
+            self.pattern_at(object_point)
+    }
+
+    fn eq_pattern(&self, other: &dyn Pattern) -> bool {
+        other
+            .as_any()
+            .downcast_ref::<Self>()
+            .map_or(false, |other| self == other)
+    }
+
+    fn clone_boxed(&self) -> Box<dyn Pattern> {
+        Box::new(self.clone())
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -30,39 +73,43 @@ impl Default for StripePattern {
 
 #[cfg(test)]
 mod tests {
-    use crate::{color::Color, pattern::StripePattern, tuples::point};
+    use crate::{
+        color::Color,
+        pattern::{Pattern, StripePattern},
+        tuples::point,
+    };
 
     #[test]
     fn creating_stripe_pattern() {
         let pattern = StripePattern::new(Color::white(), Color::black());
         assert!(pattern.a == Color::white());
-        assert!(pattern.b == Color::black())
+        assert!(pattern.b == Color::black());
     }
 
     #[test]
     fn stripe_pattern_is_constant_in_y() {
         let pattern = StripePattern::new(Color::white(), Color::black());
-        assert_eq!(pattern.stripe_at(point(0.0, 0.0, 0.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(0.0, 1.0, 0.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(0.0, 2.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 1.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 2.0, 0.0)), Color::white());
     }
 
     #[test]
     fn stripe_pattern_is_constant_in_z() {
         let pattern = StripePattern::new(Color::white(), Color::black());
-        assert_eq!(pattern.stripe_at(point(0.0, 0.0, 0.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(0.0, 0.0, 1.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(0.0, 0.0, 2.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 0.0, 1.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 0.0, 2.0)), Color::white());
     }
 
     #[test]
     fn stripe_pattern_alternates_in_x() {
         let pattern = StripePattern::new(Color::white(), Color::black());
-        assert_eq!(pattern.stripe_at(point(0.0, 0.0, 0.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(0.9, 0.0, 0.0)), Color::white());
-        assert_eq!(pattern.stripe_at(point(1.0, 0.0, 0.0)), Color::black());
-        assert_eq!(pattern.stripe_at(point(-0.1, 0.0, 0.0)), Color::black());
-        assert_eq!(pattern.stripe_at(point(-1.0, 0.0, 0.0)), Color::black());
-        assert_eq!(pattern.stripe_at(point(-1.1, 0.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.0, 0.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(0.9, 0.0, 0.0)), Color::white());
+        assert_eq!(pattern.pattern_at(point(1.0, 0.0, 0.0)), Color::black());
+        assert_eq!(pattern.pattern_at(point(-0.1, 0.0, 0.0)), Color::black());
+        assert_eq!(pattern.pattern_at(point(-1.0, 0.0, 0.0)), Color::black());
+        assert_eq!(pattern.pattern_at(point(-1.1, 0.0, 0.0)), Color::white());
     }
 }
